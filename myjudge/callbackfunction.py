637d213d9e5ff010e16a85c97914a49786210f17
Raihan Ramadistra <ramadistra@gmail.com>
2017-10-11 14:16:42 +0700

    khusus untuk fijar

--- myjudge/callbackfunction.py
+++ myjudge/callbackfunction.py
@@ -4,6 +4,9 @@ def callbackfunction(tree):
     username = tree[0][0].text
     name = tree[0][1][3].text
     first, last = name.split(" ", 1)
+    if name == "M. Fijar Lazuardy R. El Farabi":
+        first = "M. Fijar"
+        last = "Lazuardy R. El Farabi"
     user, _ = User.objects.get_or_create(username=username)
     user.first_name = first
     user.last_name = last
