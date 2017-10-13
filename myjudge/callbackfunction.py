5dee93eeffcd0641f912b8eca137d970aff2ac27
Raihan Ramadistra <ramadistra@gmail.com>
2017-10-13 16:00:07 +0700

    fixed bug where duplicate user is created due to case sensitivity

--- myjudge/callbackfunction.py
+++ myjudge/callbackfunction.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 
 def callbackfunction(tree):
-    username = tree[0][0].text
+    username = tree[0][0].text.lower()
     name = tree[0][1][3].text
     first, last = name.split(" ", 1)
     if name == "M. Fijar Lazuardy R. El Farabi":
