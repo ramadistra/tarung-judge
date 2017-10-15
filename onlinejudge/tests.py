ad18e263e91ab61f242fb5d403f589be4f31e43e
Raihan Ramadistra <ramadistra@gmail.com>
2017-10-15 17:10:27 +0700

    Added test cases for authenticated and anonymous users

--- onlinejudge/tests.py
+++ onlinejudge/tests.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
 
-from .models import Attempt
+from .models import Attempt, Question
+from . import views
 from . import judger
+
 
 class JudgerTestCase(TestCase):
     def test_parse_response(self):
@@ -31,3 +34,87 @@ class JudgerTestCase(TestCase):
             'status': 'OK'
         }
         self.assertEqual(judger.match(expected_out, response), ([correct, wrong], False))
+
+
+class PublicViewsTest(TestCase):
+    """
+    Test views that can be accessed by both 
+    anynomous and authenticated users.
+    """
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='test', email='test@case.com', password='test_case')
+        self.public_views = [
+            ('/', views.home), 
+            ('/scoreboard', views.leaderboard), 
+            ('/activity', views.activity),
+            ('/activity', views.activity),
+        ]
+
+    def test_anonymous_user(self):
+        for url, view in self.public_views:
+            request = self.factory.get(url)
+            request.user = AnonymousUser()
+            response = view(request)
+            self.assertEqual(response.status_code, 200)
+
+    def test_authenticated_user(self):
+        for url, view in self.public_views:
+            request = self.factory.get(url)
+            request.user = AnonymousUser()
+            response = view(request)
+            self.assertEqual(response.status_code, 200)
+
+
+class LoginRequiredTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='test', email='test@case.com', password='test_case')
+
+    def test_anonymous_user(self):
+        request = self.factory.get('/profile/test')
+        request.user = AnonymousUser()
+        response = views.profile(request, username='test')
+        # Users that are not logged in should be redirected to login page.
+        self.assertEqual(response.status_code, 302)
+
+    def test_authenticated_user(self):
+        request = self.factory.get('/profile/test')
+        request.user = self.user
+        response = views.profile(request, username='test')
+        self.assertEqual(response.status_code, 200)
+
+
+class QuestionViewsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='test', email='test@case.com', password='test_case')
+        self.question = Question(
+            title = "Test Question",
+            description = "This is a test.",
+            slug = "test-question",
+            difficulty = 20,
+            template = "# Hello"
+        )
+        self.question.save()
+
+    def test_question_details(self):
+        # Anonymous and authenticated users should be able to view questions.
+        for user in [self.user, AnonymousUser()]:
+            request = self.factory.get('/question/%s' % self.question.slug)
+            request.user = user
+            response = views.detail(request, self.question.slug)
+            self.assertEqual(response.status_code, 200)
+
+    def test_submit_question(self):
+        # Anonymous user cannot submit a question and will be redirected.
+        request = self.factory.post(
+            '/question/%s/submit' % self.question.slug,
+            data = {'source':'print("Test!")'}
+            )
+        request.user = AnonymousUser()
+        response = views.submit(request, self.question.slug)
+        self.assertEqual(response.url, "/login?next=/question/%s/submit" % self.question.slug)
