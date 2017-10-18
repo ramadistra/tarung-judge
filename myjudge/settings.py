f65b8a6a4eacffa10d97b73a930052d76f60fdae
Raihan Ramadistra <ramadistra@gmail.com>
2017-10-18 19:33:16 +0700

    disabled heroku test runner

--- myjudge/settings.py
+++ myjudge/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "true"
 
-TEST_RUNNER = 'myjudge.heroku_test_runner.HerokuDiscoverRunner'
+# TEST_RUNNER = 'myjudge.heroku_test_runner.HerokuDiscoverRunner'
 
 
 # Application definition
